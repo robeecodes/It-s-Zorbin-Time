20378a87bf185d196a5fe20c941cdc7796deb03c
Lily Summers <lily2.summers@live.uwe.ac.uk>
2025-01-25 15:26:18 +0000

    feat: basic character select menu

--- Assets/_Scripts/UI/UIManager.cs
+++ Assets/_Scripts/UI/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class UI : MonoBehaviour
 {
+    public PlayerSkin charSelected = PlayerSkin.NULL;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,13 +20,44 @@ public class UI : MonoBehaviour
 
     }
 
+    // Main Menu Buttons
     public void OnStartPressed()
     {
         SceneManager.LoadScene("Character Select");
     }
-
     public void OnQuitPressed()
     {
         Application.Quit();
+    }
+
+    // Character Select Buttons
+    public void OnPlayPressed()
+    {
+        if (charSelected != PlayerSkin.NULL)
+        {
+            Debug.Log(charSelected);
+            SceneManager.LoadScene("Lobby");
+        }
+    }
+    public void OnBackPressed()
+    {
+        SceneManager.LoadScene("Title Menu");
+    }
+    // player skins
+    public void DefaultSelected()
+    {
+        charSelected = PlayerSkin.Default;
+    }
+    public void RobotSelected()
+    {
+        charSelected = PlayerSkin.Robot;
+    }
+    public void SansSelected()
+    {
+        charSelected = PlayerSkin.Sans;
+    }
+    public void AmongusSelected()
+    {
+        charSelected = PlayerSkin.Amongus;
     }
 }
